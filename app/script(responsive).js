827bf2243b973a560d918e9c45c8b866375ca647
amir_rafigh <amir.rafigh.work@gmail.com>
2024-11-12 21:44:46 +0330

    Improved responsive design implementation

--- app/script(responsive).js
+++ app/script(responsive).js
@@ -1,4 +1,3 @@
-
 const bar_bar = document.querySelector('.fa-bars');
 bar_bar.addEventListener('click' , ()=>{
     const aside = document.querySelector(".sidebar");
@@ -6,47 +5,3 @@ bar_bar.addEventListener('click' , ()=>{
 })
 
 
-const smartmobile = window.matchMedia("(max-width:811px)");
-smartmobile.addEventListener('change' , handlesmartmobile);
-function handlesmartmobile(){
-    const header_main = document.querySelector(".header_main");
-    if(smartmobile.matches){
-        if(!document.querySelector('.pro_ch_bar')){
-            header_main.appendChild(insert());
-
-        }
-
-    }
-    else{
-        removePCB()
-    }
-
-}
-
-function insert(){
-    const PCB = document.createElement("div");
-    PCB.classList.add("pro_ch_bar");
-    const profile = document.querySelector('.profile');
-    const change_theme = document.querySelector('.change-theme');
-    const bar = document.querySelector(".bar");
-    PCB.appendChild(profile)
-    PCB.appendChild(change_theme)
-    PCB.appendChild(bar)
-    return PCB;
-    
-}
-
-
-
-function removePCB(){
-    const PCB = document.querySelector('.pro_ch_bar')
-    const profile = PCB.querySelector('.profile');
-    const change_theme = PCB.querySelector('.change-theme');
-    const bar = PCB.querySelector(".bar");
-    header_main.appendChild(profile)
-    header_main.appendChild(change_theme)
-    header_main.appendChild(bar)
-    PCB.remove();
-    
-
-}
--- app/script.js
+++ app/script.js
@@ -14,6 +14,52 @@ const bar = document.querySelector(".bar");
 
 
 window.addEventListener('load' , ()=>{
+    
+    const smartmobile = window.matchMedia("(max-width:811px)");
+    smartmobile.addEventListener('change' , handlesmartmobile);
+    handlesmartmobile();
+    function handlesmartmobile(){
+        const header_main = document.querySelector(".header_main");
+        if(smartmobile.matches){
+            if(!document.querySelector('.pro_ch_bar')){
+                header_main.appendChild(insert());
+
+            }
+
+        }
+        else{
+            removePCB()
+        }
+
+    }
+
+    function insert(){
+        const PCB = document.createElement("div");
+        PCB.classList.add("pro_ch_bar");
+        const profile = document.querySelector('.profile');
+        const change_theme = document.querySelector('.change-theme');
+        const bar = document.querySelector(".bar");
+        PCB.appendChild(profile)
+        PCB.appendChild(change_theme)
+        PCB.appendChild(bar)
+        return PCB;
+
+    }
+
+
+
+    function removePCB(){
+        const PCB = document.querySelector('.pro_ch_bar')
+        const profile = PCB.querySelector('.profile');
+        const change_theme = PCB.querySelector('.change-theme');
+        const bar = PCB.querySelector(".bar");
+        header_main.appendChild(profile)
+        header_main.appendChild(change_theme)
+        header_main.appendChild(bar)
+        PCB.remove();
+
+
+    }
     for( item of getitemlocal()){
         creatli(item);
     }
@@ -60,7 +106,11 @@ ul.addEventListener('click' , (x)=>{
     }
     if(x.target.nodeName === "DIV"){
         x.target.parentElement.classList.toggle('li_done');
-        x.target.classList.toggle('done_close');        
+        x.target.classList.toggle('done_close');   
+        
+       
+
+
     }
 
 })
